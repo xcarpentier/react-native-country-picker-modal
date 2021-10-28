dbfe98a183c2e852818c7d17dec2a29a3cf87e5d
TagMango <tagmango@swarajs-MacBook-Pro.local>
2021-10-28 10:05:26 +0530

    lib added

---
+++ lib/CountryModal.d.ts
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { ModalProps } from 'react-native';
+export declare const CountryModal: {
+    ({ children, withModal, disableNativeModal, ...props }: import("react-native").ModalBaseProps & import("react-native").ModalPropsIOS & import("react-native").ModalPropsAndroid & {
+        children: React.ReactNode;
+        withModal?: boolean | undefined;
+        disableNativeModal?: boolean | undefined;
+    }): JSX.Element | null;
+    defaultProps: {
+        animationType: string;
+        animated: boolean;
+        withModal: boolean;
+        disableNativeModal: boolean;
+    };
+};
---
+++ lib/CountryText.d.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+import { TextProps } from 'react-native';
+export declare const CountryText: (props: TextProps & {
+    children: ReactNode;
+}) => JSX.Element;
